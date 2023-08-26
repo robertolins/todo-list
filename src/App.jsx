7b1a0aac05ea27a4697423f58e9cda5b69dbed6f
Roberto Lima <robertolimalins@gmail.com>
2023-08-26 20:58:16 -0300

    feat: estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
+import './assets/Theme.css'
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Lista de Tarefas</h1>
-    </>
+      <form action="">
+          <input type='text' placeholder="Adicione uma tarefa" />
+          <button type="submit">Adicionar</button>
+      </form>
+      <div className='listaTarefas'>
+        <div className='item'>
+            <span>A tarefa ficará aqui</span>
+            <button>Excluir</button>
+        </div>
+        <div className='item completo'>
+            <span>A tarefa ficará aqui</span>
+            <button>Excluir</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
