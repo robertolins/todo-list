242385f0eaeedb6797370318e1b3a1595fadd27e
Roberto Lima <robertolimalins@gmail.com>
2023-08-26 21:53:03 -0300

    feat: adicionando e removendo tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,66 @@
-import { useState } from 'react'
+import react, { useState } from 'react'
 import './assets/Theme.css'
 
 function App() {
+  const [list, setList] = useState([])
+  const [descTask, setDescTask] = useState('')
+
+  const addTask = form => {
+    form.preventDefault()
+
+    if (!descTask) return
+    const newTask = { description: descTask, isCompleted: false }
+    setList([...list, newTask])
+    setDescTask('')
+  }
+
+  const completeTask = index => {
+    const listAux = [...list]
+    listAux[index].isCompleted = !listAux[index].isCompleted
+    setList(listAux)
+  }
+
+  const removeTask = index => {
+    const listAux = [...list]
+    listAux.splice(index, 1)
+    setList(listAux)
+  }
+
+  const removeAll = () => setList([])
+
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <form action="">
-          <input type='text' placeholder="Adicione uma tarefa" />
+      <form onSubmit={addTask}>
+          <input 
+            type='text' 
+            placeholder="Adicione uma tarefa" 
+            value={ descTask }
+            onChange={(e) => setDescTask(e.target.value)}
+          />
           <button type="submit">Adicionar</button>
       </form>
-      <div className='listaTarefas'>
-        <div className='item'>
-            <span>A tarefa ficará aqui</span>
-            <button>Excluir</button>
-        </div>
-        <div className='item completo'>
-            <span>A tarefa ficará aqui</span>
-            <button>Excluir</button>
-        </div>
+      <div>
+        {
+          list.length < 1 
+            ?
+            <div>
+              <span>Lista Vazia</span>
+            </div>
+            :
+            list.map((item, index) => (
+              <div key={ index } className={ item.isCompleted ? 'completed' : ''}>
+                <span onClick={ () => { completeTask(index) } }>{ item.description }</span>
+                <button onClick={ () => { removeTask(index) } }>Excluir</button>
+              </div>
+            ))
+        }
+        {
+          list.length > 0 && 
+          <div>
+            <button onClick={ () => { removeAll() } }>Excluir Tudo</button>
+          </div>
+        }
       </div>
     </div>
   )
